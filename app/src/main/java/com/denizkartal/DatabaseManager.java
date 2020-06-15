44ef42a5cd02850446361622d64cdbeea12488c0
Deniz Kartal <denizcankartal@hotmail.com>
2020-06-15 23:33:06 +0000

    Update app/src/main/java/com/denizkartal/Plant.java, app/src/main/java/com/denizkartal/DatabaseManager.java files

---
+++ app/src/main/java/com/denizkartal/DatabaseManager.java
@@ -0,0 +1,15 @@
+public class DatabaseManager{
+    
+    public static void connectToDB(){
+
+    }
+    public static void addToDB(){
+
+    }
+    public static void readFromDB(){
+
+    }
+    public static void deleteFromDB(){
+        
+    }
+}
