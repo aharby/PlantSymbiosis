17a2d5b8b825776c9b08ae62932b9651f7c3577e
poseidon42 <denizcankartal@hotmail.com>
2020-06-21 20:15:23 +0200

    databasemanager, add plant to db

--- app/src/main/java/com/davidriad/se/project/se_project_grp8/AddPlant.java
+++ app/src/main/java/com/davidriad/se/project/se_project_grp8/AddPlant.java
@@ -3,37 +3,52 @@ package com.davidriad.se.project.se_project_grp8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class AddPlant extends AppCompatActivity {
 
+    Button btnAdd;
+    TextView textViewPlantName;
+    DatabaseReference databaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_plant);
 
-        final Button button = findViewById(R.id.button);
-        final TextView textViewPlantID = findViewById(R.id.editTextTextPersonName);
-        final TextView textViewPlantName = findViewById(R.id.editTextTextPersonName2);
+        databaseReference = FirebaseDatabase.getInstance().getReference("plants");
 
-        button.setOnClickListener(new View.OnClickListener() {
+        textViewPlantName = (TextView) findViewById(R.id.editTextPlantName);
+        btnAdd = (Button) findViewById(R.id.buttonAdd);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String plantID = (String) textViewPlantID.getText();
-                String plantName = (String) textViewPlantName.getText();
-
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference databaseReference = database.getReference("/plants");
-
-                PlantModel plant = new PlantModel(plantID, plantName, "description");
-
-                databaseReference.setValue(plant);
+                addPlant();
             }
         });
+    }
+    private void addPlant(){
+        String plantName = textViewPlantName.getText().toString();
+
+        if(!TextUtils.isEmpty(plantName)){
+            // get a unique ID
+            String plantID = databaseReference.push().getKey();
+
+            PlantModel plant = new PlantModel(plantID,plantName, "DescriptionTest");
+
+            databaseReference.child(plantID).setValue(plant);
+
+            Toast.makeText(this, "Success", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(this,"Please enter a plant name", Toast.LENGTH_LONG).show();
+        }
     }
 }
