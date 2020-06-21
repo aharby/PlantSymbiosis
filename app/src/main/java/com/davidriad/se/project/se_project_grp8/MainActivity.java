5b11b89a56ecd755e47c4f5e6ad6d32456dafdf0
DavidRiad <davidmagdynawar@gmail.com>
2020-06-21 18:37:58 +0200

    solving conflicts

--- app/src/main/java/com/davidriad/se/project/se_project_grp8/MainActivity.java
+++ app/src/main/java/com/davidriad/se/project/se_project_grp8/MainActivity.java
@@ -1,6 +1,9 @@
 package com.davidriad.se.project.se_project_grp8;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,17 +32,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FirebaseDatabase plantDatabase = FirebaseDatabase.getInstance();
-         plantDBRef = plantDatabase.getReference("/plants");
+        plantDBRef = plantDatabase.getReference("/plants");
 
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        EditText search = findViewById(R.id.search);
         LinearLayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.VERTICAL,false);
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
-        mAdapter = new PlantsAdapter(plantsList,getApplicationContext());
+        mAdapter = new PlantsAdapter(plantsList,this);
         recyclerView.setAdapter(mAdapter);
         preparePlantData();
 
+
+        search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                filter(s.toString());
+
+            }
+        });
+    }
+
+    private void filter(String text) {
+        ArrayList<PlantModel> filteredList = new ArrayList<> ();
+        for (PlantModel plant : plantsList) {
+            if ((plant.getName().toLowerCase())
+                    .contains(text.toLowerCase())) {
+                filteredList.add(plant);
+            }
+        }
+        mAdapter.filterList(filteredList);
     }
 
 
